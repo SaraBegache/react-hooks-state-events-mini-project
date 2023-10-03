e16d9809872f66ddbc703afe9271f7d9810f8609
Sara BEGACHE <sara.begache@gmail.com>
2023-10-03 22:34:21 +0100

    Completed Assignement

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,12 +1,38 @@
 import React from "react";
+import React, { useState } from "react";
+
+function CategoryFilter({ categories, setButtonCategory }) {
+  const [isSelected, setIsSelected] = useState(
+    categories.map((category) => ({
+      [category]: false,
+    }))
+  );
+  // console.log(isSelected);
+
+  function handleButtonClick(e) {
+    const name = e.target.name;
+    const newButtons = [...isSelected];
+    newButtons[name] = !newButtons[name];
+    setIsSelected(newButtons);
+    setButtonCategory(name);
+  }
 
 function CategoryFilter() {
   return (
     <div className="categories">
       <h5>Category filters</h5>
       {/* render <button> elements for each category here */}
+      {categories.map((category) => (
+        <button
+          className={isSelected[category] ? "selected" : ""}
+          name={category}
+          key={category}
+          onClick={handleButtonClick}
+        >
+          {category}
+        </button>
+      ))}
     </div>
   );
 }
-
 export default CategoryFilter;
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,20 @@
 import React from "react";
+import Task from "./Task";
 
 function TaskList() {
+function TaskList({ tasks, handleDelete }) {
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      {tasks.map(({ text, category }, i) => (
+        <Task
+          key={i + text}
+          text={text}
+          category={category}
+          handleDelete={() => handleDelete(text)}
+        />
+      ))}
     </div>
   );
 }
-
 export default TaskList;
